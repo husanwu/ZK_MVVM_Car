9b8f46ce4d1bd2da23bf9b292707e1ca321c56d7
Bo-Xuan Fan <coffee0127@gmail.com>
2017-06-27 21:59:54 +0800

    code refactor 1. 使用 lombok 產生 getter & setter 2. 呼叫多參數建構子

--- ZK_MVVM_Car/src/main/java/com/mvvm/model/Car.java
+++ ZK_MVVM_Car/src/main/java/com/mvvm/model/Car.java
@@ -1,5 +1,11 @@
 package com.mvvm.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+// getter & setter 交由 lombok 產生
+@Getter
+@Setter
 public class Car {
 
 	private Integer id;
@@ -10,8 +16,11 @@ public class Car {
 	private Integer price;
 
 	public Car() {
+	    // 建構子應由參數少的呼叫參數多的
+	    this(null, null, null, null, null, null);
 	}
 
+	// 建構子參數太多 is not a best practice
 	public Car(Integer id, String model, String make, String description, String preview, Integer price) {
 		this.id = id;
 		this.model = model;
@@ -21,51 +30,4 @@ public class Car {
 		this.price = price;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getMake() {
-		return make;
-	}
-
-	public void setMake(String make) {
-		this.make = make;
-	}
-
-	public String getPreview() {
-		return preview;
-	}
-
-	public void setPreview(String preview) {
-		this.preview = preview;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public String getModel() {
-		return model;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
 }
