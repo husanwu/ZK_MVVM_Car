0b171c382de1abb99af1553d12e58b30ac88a0a1
巫芸瑄 <yunwu0128@gmail.com>
2017-06-29 10:41:09 +0800

    paging、forEachStatus、Command binding that pass parameters
    1.paging
      - 透過mold="paging" pageSize="3"屬性設定分頁
    
    2.forEachStatus
      - retrieve the iteration's index via 	${forEachStatus.index}
        設定集合的序號
      
    3.@BindingParam
      - 前端使用@command(key1="value", key2=object)綁定參數
      - ViewModel裡的方法參數為(@BindingParam("key1") String str,
                                @BindingParam("key2") Object obj)

--- ZK_MVVM_Car/src/main/java/com/mvvm/viewmodel/SearchViewModel.java
+++ ZK_MVVM_Car/src/main/java/com/mvvm/viewmodel/SearchViewModel.java
@@ -2,6 +2,7 @@ package com.mvvm.viewmodel;
 
 import java.util.List;
 
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.event.Event;
@@ -30,25 +31,32 @@ public class SearchViewModel {
 		carList = carService.search(keyword);
 		//執行新的查詢時，清空selectedCar
 		selectedCar = null;
+		//執行新的查詢時，隱藏修改區塊
 		upVisible = false;
 	}
 
-    public void delete() {
-        carService.delete(selectedCar);
-        //刪除selectedCar時，清空selectedCar，並重新載入Car清單
+    /*
+     * 前端使用@command(key1="value", key2=object)綁定參數
+     * ViewModel裡的方法參數為(@BindingParam("key1") String str, @BindingParam("key2") Object obj)
+    */
+    @Command
+    @NotifyChange({"carList", "selectedCar"})
+    public void delete(@BindingParam("thiscar") Car car) {
+        carService.delete(car);
+        //刪除car時，清空selectedCar，並重新載入Car清單
         carList = carService.search(keyword);
         selectedCar = null;
     }
 
-    @NotifyChange({"carList", "selectedCar"})
     @Command
-    public void deleteMsg() {
+    @NotifyChange({"carList", "selectedCar"})
+    public void deleteMsg(@BindingParam("thiscar") Car car) {
         Messagebox.show("確定刪除?", "刪除", Messagebox.OK | Messagebox.CANCEL, Messagebox.EXCLAMATION,
                             new org.zkoss.zk.ui.event.EventListener() {
             @Override
             public void onEvent(Event evt) throws Exception {
                 if (evt.getName().equals("onOK")) {
-                    carService.delete(selectedCar);
+                    carService.delete(car);
                     carList = carService.search(keyword);
                     selectedCar = null;
                 }
@@ -62,14 +70,15 @@ public class SearchViewModel {
         carService.update(i, selectedCar);
     }
 
-    @NotifyChange("upVisible")
+    //設定修改區塊Visible
     @Command
+    @NotifyChange("upVisible")
     public void showUpdate() {
         upVisible = true;
     }
 
-    @NotifyChange("upVisible")
     @Command
+    @NotifyChange("upVisible")
     public void hideUpdate() {
         upVisible = false;
     }
