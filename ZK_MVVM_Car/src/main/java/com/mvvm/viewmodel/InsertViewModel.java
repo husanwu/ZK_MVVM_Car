14f97d33321357d399e894983dfec796329dd726
巫芸瑄 <yunwu0128@gmail.com>
2017-06-29 16:17:03 +0800

    Revert "upload image"
    This reverts commit 490cf1301b8b0202a586ffc38362777d79627130.

--- ZK_MVVM_Car/src/main/java/com/mvvm/viewmodel/InsertViewModel.java
+++ ZK_MVVM_Car/src/main/java/com/mvvm/viewmodel/InsertViewModel.java
@@ -1,13 +1,6 @@
 package com.mvvm.viewmodel;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
-import org.zkoss.util.media.Media;
 
 import com.mvvm.model.Car;
 import com.mvvm.model.CarService;
@@ -26,30 +19,6 @@ public class InsertViewModel {
     @Command
     public void insert() {
         carService.insert(car);
-    }
-
-    @Command
-    public void uploadImg(@BindingParam("media") Media media) throws Exception {
-
-        if (media != null && media instanceof org.zkoss.image.Image) {
-
-                String fileName = media.getName();
-                String imgPath = "/widgets/getting_started/img/" + fileName;
-                car.setPreview(imgPath);
-
-                File file = new File("D:/Project/repository/ZK_MVVM_Car/src/main/webapp/widgets/getting_started/img/" + fileName);
-                OutputStream out = new FileOutputStream(file);
-                InputStream in = media.getStreamData();
-                byte[] buffer = new byte[1024];
-                int len;
-
-                while ((len = in.read(buffer)) != -1 ) {
-                    out.write(buffer, 0, len);
-                }
-                out.close();
-                in.close();
-        }
-
     }
 
 }
